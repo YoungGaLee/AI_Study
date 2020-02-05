b7a99b8d5697f7608481ad5f652f05d503576917
gek7537 <56713634+gek7537@users.noreply.github.com>
2020-02-05 15:43:35 +0900

    Create Brain_classification_CJA.py

---
+++ [실습_2] Brain_classification/Brain_classification_CJA.py
@@ -0,0 +1,77 @@
+import pandas as pd
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+#시드고정
+torch.manual_seed(1)
+
+#파라미터
+learning_rate = 0.001
+nb_epochs = 1000
+
+#데이터로드 및 셔플
+al_data = pd.read_excel('/content/drive/My Drive/인공지능 두뇌지수 데이터_1024.xlsx', usecols=['Scholarly Output','Most recent publication','Citations','Citations per Publication','Field-Weighted Citation Impact','h-index','Country Number','Scholarly Output100','Citations per Publication100','Field-Weighted Citation Impact100','Degree'])
+xy_data = al_data.values
+
+np.random.shuffle(xy_data)
+
+#print(al_data)
+#print(xy_data)
+
+#데이터 구분
+x_data = xy_data[:,0:-1]
+y_data = xy_data[:,[-1]]
+
+#데이터 텐서로 변환
+x_train = torch.FloatTensor(x_data)
+y_train = torch.LongTensor(y_data)
+
+
+#x 데이터 값 정규화
+mu = x_train.mean(dim=0)
+sigma = x_train.std(dim=0)
+norm_x_train = (x_train - mu) / sigma
+
+#print(norm_x_train)
+print(norm_x_train.shape)
+print(y_train.shape)
+
+#x_data.dtype
+y_data.dtype
+
+#검증데이터 테스트데이터 구분
+
+#모델생성
+
+linear1 = torch.nn.Linear(10, 256, bias=True)
+linear2 = torch.nn.Linear(256, 25, bias=True)
+relu = torch.nn.ReLU()
+
+model = torch.nn.Sequential(linear1, relu, linear2)
+#print(model)
+
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+for epoch in range(nb_epochs + 1):
+    hypothesis = F.softmax(model(x_train), dim = 1)
+    #y_one_hot = torch.zeros_like(hypothesis)
+    #y_one_hot.scatter_(1, y_train.unsqueeze(1), 1)
+
+    #cost = (y_one_hot * -torch.log(F.softmax(hypothesis, dim=1))).sum(dim=1).mean()
+    cost = F.cross_entropy(hypothesis, y_train)
+    #cost = torch.nn.CrossEntropyLoss(hypothesis, y_train)
+
+
+    # cost로 H(x) 개선
+    optimizer.zero_grad()
+    cost.backward()
+    optimizer.step()
+
+    # 10번마다 로그 출력
+    if epoch % 100 == 0:
+        print('Epoch {:4d}/{} Cost: {:.6f}'.format(
+            epoch, nb_epochs, cost.item()
+        ))
